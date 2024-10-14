9907d2467aeac8f3cf0ab2b52d40f417b9744980
unknown <joshuamackie10@googlemail.com>
2024-10-14 16:03:11 +0100

    created the API call to check data of benchmark data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const apiUrl = "https://substantive.pythonanywhere.com";
+  const endPoint = "/product_benchmarks";
+  const Headers = {
+    "auth-key" : "590e3e17b6a26a8fcda726e2a91520e476e2c894",
+  };
+  const [productBenchmarks, setProductBenchmarks] = useState();
+
+  const getProductBenchmarks = () => {
+    const url = apiUrl + endPoint;
+    fetch(url, {
+      method: "GET",
+      headers: Headers
+    })
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error("Network Response was not Ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProductBenchmarks(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+      });
+  };
+
+  useEffect(() => {
+    getProductBenchmarks();
+  }, []);
 
   return (
     <>
      <h1 class="text-red-500">Hello World</h1>
+     <div>
+      {productBenchmarks ? (
+        <pre>{JSON.stringify(productBenchmarks, null, 2)}</pre>
+      ) : (
+        <p>loading...</p>
+      )}
+     </div>
     </>
   )
 }
