3770cdd9f6680786a6217b692e9426990585a850
unknown <joshuamackie10@googlemail.com>
2024-10-20 16:56:04 +0100

    created currency logic for converting when necessary currency to Euros

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 import { useEffect, useState } from 'react'
 import { productInfo, currencyInfo } from "./apis/api.jsx";
+import { convertProductToEUR } from "./currency/currency.jsx"
+
 
 function App() {
-  const [currencyData, setCurrencyData] = useState(null);
-  const [productData, setProductData] = useState(null);
+  const [currencyData, setCurrencyData] = useState([]);
+  const [productData, setProductData] = useState([]);
+
+  
+ 
+
 
   useEffect(() => {
     
     const fetchCurrencyInfo = async () => {
       const data = await currencyInfo();
-      setCurrencyData(data);
+      setCurrencyData(data.exchange_rates);
     };
 
     const fetchProductInfo = async () => {
       const data = await productInfo();
-      setProductData(data)
-    }
-
-
+      setProductData(data.product_benchmarks);
+    };
     fetchCurrencyInfo();
     fetchProductInfo();
   }, []);
 
+  const handleTestConversion = () => {
+    if (currencyData.length > 0 && productData.length > 0) {
+      const productToTest = productData[1];
+      const convertedProduct = convertProductToEUR(productToTest, currencyData);
+
+      console.log("Original Product: ", productToTest);
+      console.log("Converted Product: ", convertedProduct);
+    } else {
+      console.log("Data is still loading...");
+    } 
+  };
+
   return (
     <>
      <h1 className="text-red-500">Hello World</h1>
+     <div>
+      <h1>Currency Conversion Test</h1>
+      <button onClick={handleTestConversion}>Test Conversion</button>
+     </div>
      <div>
       {currencyData ? (
         <pre>{JSON.stringify(currencyData, null, 2)}</pre>
